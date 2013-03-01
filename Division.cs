72ff83b35b1a74e2bf56f9f3a02ce5d0238531b8
TimothyHeermann <musicalglass@hotmail.com>
2013-02-28 17:05:07 -0800

    uploaded Math Quiz apps

---
+++ Division.cs
@@ -0,0 +1,325 @@
+using System;
+using System.Collections.Generic;
+using System.Media;
+
+public class AdditionQuiz
+{
+    static void Main()
+    {
+
+    #region Local Declarations
+
+        const int ArraySize = 10;
+        int SwapSpace = 0;
+        bool ToggleTimer = true;
+        DateTime startTime = DateTime.Now;
+        int UserAnswer = 0;
+        double AverageTime = 0.0;
+
+        //create a 10 x 10 2-Dimensional Array 0 to 9, for Elapsed Time
+        double[,] MultiTable = new double[ArraySize, ArraySize];
+
+        // Create 2, one dimensional arrays for Multiplicands, Multiplyers
+        int[] Multiplicands = new int[ArraySize * ArraySize];
+        int[] Multiplyers = new int[ArraySize * ArraySize];
+
+        List<int> myQueue = new List<int>();
+
+        // Declare an Instance of Random Class outside the Loop
+        Random MyRandNumb = new Random();
+
+        #endregion
+
+    #region Intro
+
+        Console.WriteLine("\n\nWelcome to Dillinger's Math Quiz program.\n");
+
+        System.Threading.Thread.Sleep(3000);
+
+        Console.WriteLine("First let's go through the Division Tables");
+        Console.WriteLine("and calculate your average response time.\n");
+
+        System.Threading.Thread.Sleep(4000);
+
+        Console.WriteLine("Press the Enter key to Begin.");
+        Console.ReadLine();
+
+        #endregion
+
+    #region Seed the Arrays
+
+        // Fill Multiplicands and Multiplyers Arrays with seed numbers
+        for (int hCounter = 0; hCounter < ArraySize; hCounter++)
+        {
+            for (int iCounter = (hCounter * ArraySize); iCounter < ((hCounter * ArraySize) + ArraySize); iCounter++)
+            {  //  0000000000, 1111111111, 222... etc.
+                Multiplicands[iCounter] = hCounter;
+            }
+        }
+
+        for (int hCounter = 0; hCounter < ArraySize; hCounter++)
+        {   // 0123456789, 0123456789, 0123... etc.
+            for (int iCounter = 0; iCounter < (ArraySize); iCounter++)
+            {
+                Multiplyers[iCounter + (hCounter * ArraySize)] = iCounter;
+            }
+        }
+
+        // Number the Queue
+        for (int iCounter = 0; iCounter < ArraySize * ArraySize; iCounter++)
+        {
+            myQueue.Add(iCounter); // Enqueue
+        } 
+
+        // end Seed Arrays
+        #endregion
+
+    #region Shuffle Loosely
+        //Shuffle the Queue in 4 separate overlapping segments so it starts easy and gets harder
+        // 0-27  18-45 36-63 54-81
+        // 0 - 3   2 - 5   4 - 7    6 - 9      ( * 9 or SizeOfArray )
+        for (int iCounter = 0; iCounter < 9; iCounter++)
+        {
+            for (int kCounter = ((iCounter * 9) + 1); kCounter < (((iCounter + 3) * 9) + 1); kCounter++)
+            {
+                // Generate Random number within current boundaries
+                int Swap = MyRandNumb.Next((1 + (iCounter * 9)), ((iCounter + 3) * 9));
+
+                SwapSpace = myQueue[kCounter]; // Store each cell temporarily in SwapSpace Variable
+                myQueue[kCounter] = myQueue[Swap]; // Swap two random cells
+                myQueue[Swap] = SwapSpace; // Put SwapSpace Variable back in leftover cell
+            }
+        } // end Shuffle
+        #endregion
+
+        #region Remove Divide by Zero // ( Strictly a no-no )
+        // Step through the Queue
+        for (int iCounter = 0; iCounter < (myQueue.Count); iCounter++)
+        {
+		if (Multiplicands[myQueue[iCounter]] == 0)
+        {
+            myQueue.RemoveAt(iCounter);
+            iCounter--;
+	    }
+	
+        } 
+        #endregion
+
+    #region Division Quiz
+        //Use a For Loop to Quiz the user based on the Current Queue Number
+        // and print it to the screen
+        for (int jCounter = 0; jCounter < (myQueue.Count); jCounter++)
+        {
+            
+        StartHere:
+            Console.Write("\n{0} \\ {1} = ",
+                Multiplicands[myQueue[jCounter]] * Multiplyers[myQueue[jCounter]], Multiplicands[myQueue[jCounter]]);
+
+            // Start the Timer
+            if (ToggleTimer == true)
+            {
+                startTime = DateTime.Now;
+            }
+
+            try
+            {   // Convert User Input to numerical Integer
+                UserAnswer = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                SystemSounds.Exclamation.Play();
+                Console.WriteLine("\nThat does not compute!\n ");
+                Console.WriteLine("Expected whole number input. ");
+                Console.WriteLine("Please try again.\n ");
+                goto StartHere;
+            }
+
+            if (UserAnswer == (Multiplyers[myQueue[jCounter]]))
+            {   // if user answer is correct...
+                DateTime stopTime = DateTime.Now; // get the current time
+                TimeSpan TimeElapsed = (stopTime - startTime); // Calculate elapsed time
+                Console.Write("Elapsed Time:  ");
+                Console.WriteLine(TimeElapsed.TotalSeconds);
+                ToggleTimer = true;
+                //Store the elapsed time of each answer in the MultiTable Array
+                MultiTable[Multiplicands[myQueue[jCounter]], Multiplyers[myQueue[jCounter]]] = TimeElapsed.TotalSeconds;
+            }
+            else
+            {
+                SystemSounds.Beep.Play();
+                Console.Beep(100, 500);
+                Console.WriteLine("\nNope, try again");
+                ToggleTimer = false;
+                jCounter--;
+            }
+        } // end Division Quiz
+
+        #endregion
+
+    #region Average Reaction Time
+
+        // Calculate the average answer time
+        AverageTime = 0.0;  // Should be zeroed but let's make sure
+        foreach (double elementValue in MultiTable) // Read entire 2 Dim Array using Foreach
+        {
+            AverageTime = AverageTime + elementValue;  // Add all the values together
+        }
+
+        SystemSounds.Asterisk.Play();
+        Console.WriteLine("\n\nGood!\n");
+
+        System.Threading.Thread.Sleep(2000);
+
+        Console.Write("\nYour average response time was: ");
+        // Divide total by the number of Array Cells and Voila!
+        AverageTime = AverageTime / (ArraySize * ArraySize); // Store the Average
+        Console.WriteLine("{0} seconds.\n", AverageTime); // Print it
+
+        System.Threading.Thread.Sleep(3000);
+
+        Console.WriteLine("Press the Enter key to view your Reaction Times.\n");
+        Console.ReadLine();
+
+        // end Average
+        #endregion
+
+    #region UnShuffle the Queue
+
+        for (int iCounter = 0; iCounter < ArraySize * ArraySize; iCounter++)
+        {
+            myQueue.Add(iCounter); // Enqueue
+        }
+        
+        // Remove Dividing Zeros
+        for (int iCounter = 0; iCounter < (myQueue.Count); iCounter++)
+        {
+            if (Multiplicands[myQueue[iCounter]] == 0)
+            {
+                myQueue.RemoveAt(iCounter);
+                iCounter--;
+            }
+        }
+        #endregion
+
+    #region Print Reaction Times
+        Console.WriteLine("\nList of Reaction Times \n");
+        for (int iCounter = 0; iCounter < (myQueue.Count); iCounter++)
+        {
+            Console.WriteLine("{0} \\ {1}: {2} ",
+        Multiplicands[myQueue[iCounter]] * Multiplyers[myQueue[iCounter]], Multiplicands[myQueue[iCounter]], MultiTable[Multiplicands[myQueue[iCounter]], Multiplyers[myQueue[iCounter]]]);
+        }
+        #endregion
+
+    #region Intro Part 2
+        // Begin new quiz using slower than average times
+        System.Threading.Thread.Sleep(6000);
+        Console.WriteLine("\nLet's focus on the ones that took longer than average: \n");
+
+        System.Threading.Thread.Sleep(3000);
+
+        Console.WriteLine("Press the Enter key to continue");
+        Console.ReadLine(); 
+        #endregion
+
+    #region Remove the Easy Questions
+
+            for (int iCounter = 0; iCounter < (myQueue.Count); iCounter++)
+            {
+                // Step through the MultiTable
+                // If Elapsed time is Less than Average Time, Delete the reference to it
+                if ((MultiTable[Multiplicands[myQueue[iCounter]], Multiplyers[myQueue[iCounter]]] < AverageTime))
+                {
+                    myQueue.RemoveAt(iCounter);
+                    iCounter--;
+                }
+            }
+            // end free lunch
+            #endregion
+
+            while (myQueue.Count > 0)
+            {
+            #region Shuffle Random
+
+            // Shuffle the Queue
+            for (int iCounter = 0; iCounter < myQueue.Count; iCounter++)
+            {
+                // Generate Random number within current boundaries
+                int Swap = MyRandNumb.Next(myQueue.Count);
+
+                SwapSpace = myQueue[iCounter]; // Store each cell temporarily in SwapSpace Variable
+                myQueue[iCounter] = myQueue[Swap]; // Swap two random cells
+                myQueue[Swap] = SwapSpace; // Put SwapSpace Variable back in leftover cell
+
+            } // end Shuffle
+            #endregion
+
+            #region Retry Multiplication Quiz
+
+            for (int iCounter = 0; iCounter < (myQueue.Count); iCounter++)
+            {
+
+            StartHere2:
+                Console.Write("\n\n{0} \\ {1} = ",
+                    Multiplicands[myQueue[iCounter]] * Multiplyers[myQueue[iCounter]], Multiplicands[myQueue[iCounter]]);
+
+                // Start the Timer
+                if (ToggleTimer == true)
+                {
+                    startTime = DateTime.Now;
+                }
+
+                try
+                {
+                    UserAnswer = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    SystemSounds.Beep.Play();
+                    Console.Beep(100, 500);
+                    Console.WriteLine("\nThat does not compute!\n ");
+                    Console.WriteLine("Expected whole number input. ");
+                    Console.WriteLine("Please try again.\n ");
+                    goto StartHere2;
+                }
+
+                if (UserAnswer == (Multiplyers[myQueue[iCounter]]))
+                {
+                    DateTime stopTime = DateTime.Now;
+                    TimeSpan TimeElapsed = (stopTime - startTime);
+                    Console.Write("Elapsed Time:  ");
+                    Console.WriteLine(TimeElapsed.TotalSeconds);
+                    Console.Write("Previous Elapsed Time:  ");
+                    Console.WriteLine(MultiTable[Multiplicands[myQueue[iCounter]], Multiplyers[myQueue[iCounter]]]);
+                    ToggleTimer = true;
+                    // Subtract the difference from user's previous answer
+                    if (TimeElapsed.TotalSeconds < MultiTable[Multiplicands[myQueue[iCounter]], Multiplyers[myQueue[iCounter]]])
+                    {
+                        SystemSounds.Exclamation.Play();
+                        Console.Write("You were {0} seconds faster this time!\n",
+                        MultiTable[Multiplicands[myQueue[iCounter]], Multiplyers[myQueue[iCounter]]] -
+                        TimeElapsed.TotalSeconds);
+                        myQueue.RemoveAt(iCounter);
+                    }
+                }
+                else
+                {
+                    SystemSounds.Beep.Play();
+                    Console.Beep(100, 500);
+                    Console.WriteLine("\nNope, try again");
+                    ToggleTimer = false;
+                    goto StartHere2;
+                }
+            } // end if User Answer Correct
+
+            #endregion // end Retry Division Tables 
+        }
+
+    #region The End
+                SystemSounds.Asterisk.Play();
+                Console.WriteLine("\nYou have succesfully beat all of your intial slower reaction times! :)");
+                Console.WriteLine("Press the Enter key to terminate.");
+                Console.ReadLine(); 
+                #endregion // The end of The End
+
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+Dillinger's Math Quiz Programs
+
+These 4 simple applications are Math Table quiz programs.
+I wrote these purely for my own purposes to help me isolate areas in math where I was slow.
+The first phase runs you through the complete Math Tables, 100 questions in all. It will clock your reaction time on each answer. After you have completed the quiz, it will print out your times, and give you the option to go over the ones you are having trouble with. This allows you to focus more on problems that you need to work on. 
+Your scores may surprise you. For instance I noted that I was slow on multiplying by 0 because every time I would see a problem like this: 5 * 0 =
+I would say to myself "0 times any number always equals 0" mentally before writing the answer. That burns time. I had to unlearn that habit.
+
+The programs are made as simple console apps for maximum timing efficiency, especially if run under a Windows free Operating System environment, such as a DOS only computer. You can get millisecond accuracy if you cut Windows out of the picture.
+
+The folder contains both the running Windows Console Apps as well as the C# source files.
